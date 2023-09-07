ffd2ce9a2d9a1c6e1ef42f705066d7becda4fae4
Jayesh Bhoot <jayesh@bhoot.sh>
2023-09-07 11:32:42 +0530

    Update description

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Problem
 
-`<Subscribe>` wraps the two screens of the app. So any subscriptions made in both these screens should have been retained. But that does not happen.
+`<Subscribe>` wraps two screens - `<SelectCropScreen>` and `<One>`. Check the `Yield.jsx` file to verify.
+
+So any subscriptions made in both these screens - `<SelectCropScreen>` and `<One>` - should have been retained. But that does not happen.
 
 `<Subscribe source$>` works as intended though.
--- src/components/One.jsx
+++ src/components/One.jsx
@@ -5,7 +5,10 @@ export function One() {
         <h2>Review your inputs</h2>
         <div className="stack">
             <h3 className="error">Please read the following to understand the issue:</h3>
-            <p><code>&lt;Subscribe&gt;</code> wraps both this screen and the previous screen.  So any subscriptions made in both these screens should have been retained.  So the crop selected in the previous screen should have shown up here, but it does not. </p>
+            <p><code>&lt;Subscribe&gt;</code> wraps this screen (<code>&lt;One&gt;</code>) and the previous screen (<code>&lt;SelectCropScreen&gt;</code>). Check <code>Yield.jsx</code> to verify it.</p>
+
+            <p>Therefore, any subscriptions made in both these screens - <code>&lt;One&gt;</code> and <code>&lt;SelectCropScreen&gt;</code> - should have been retained.  So the crop selected in the previous screen should have shown up here. But it does not. </p>
+
             <p><strong><code>&lt;Subscribe source$&gt;</code> works as intended though.</strong></p>
         </div>
         <ReviewBox />
